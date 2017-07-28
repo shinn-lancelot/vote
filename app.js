7508cf89b488e6fb3036a4db0b3fcb46b58effb2
shinn lancelot <945226793@qq.com>
2017-07-28 16:28:16 +0800

    1.添加nodemon，用于服务端自动重启 2.路由、视图目录分模块管理

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var stylus = require('stylus');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/home/index');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,7 +40,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('common/error');
 });
 
 module.exports = app;
